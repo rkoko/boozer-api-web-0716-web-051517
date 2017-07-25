3f2f6411687b5755ffc022bdee8b83aeece2a122
rkoko <rinda.ko@gmail.com>
2017-07-25 18:02:30 -0400

    user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :ingredients
       resources :cocktails
+      post '/auth', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
     end
   end
 end
